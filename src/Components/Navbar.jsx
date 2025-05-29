b204045dc0824853a7a0e9ccac659e3c8a4c60a4
Jona <jonathan.emmanuel604@gmail.com>
2025-05-29 00:09:23 -0600

    react-section: Fun facts about Reactc: ordered list

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav className="navestilo">
+        <img className="ReactIcono" src="/ReactIcon.png" />
+        <h1 className="reacth1">ReactFacts</h1>
+      </nav>
+    </header>
+  );
+}
